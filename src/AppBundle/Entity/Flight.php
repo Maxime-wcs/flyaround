24e743ea7573aa6e7570f2e42d84766cd7e38a10
Maxime <xm.mx@laposte.net>
2017-11-23 17:55:51 +0100

    quete 2

---
+++ src/AppBundle/Entity/Flight.php
@@ -0,0 +1,345 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Flight
+ *
+ * @ORM\Table(name="flight")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FlightRepository")
+ */
+class Flight
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="departure", type="integer")
+     */
+    private $departure;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="arrival", type="integer")
+     */
+    private $arrival;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nbFreeSeats", type="smallint")
+     */
+    private $nbFreeSeats;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="seatPrice", type="float")
+     */
+    private $seatPrice;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="takeOffTime", type="datetime")
+     */
+    private $takeOffTime;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publicationDate", type="datetime")
+     */
+    private $publicationDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="pilot", type="integer")
+     */
+    private $pilot;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="plane", type="integer")
+     */
+    private $plane;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="wasDone", type="boolean")
+     */
+    private $wasDone;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set departure
+     *
+     * @param integer $departure
+     *
+     * @return Flight
+     */
+    public function setDeparture($departure)
+    {
+        $this->departure = $departure;
+
+        return $this;
+    }
+
+    /**
+     * Get departure
+     *
+     * @return int
+     */
+    public function getDeparture()
+    {
+        return $this->departure;
+    }
+
+    /**
+     * Set arrival
+     *
+     * @param integer $arrival
+     *
+     * @return Flight
+     */
+    public function setArrival($arrival)
+    {
+        $this->arrival = $arrival;
+
+        return $this;
+    }
+
+    /**
+     * Get arrival
+     *
+     * @return int
+     */
+    public function getArrival()
+    {
+        return $this->arrival;
+    }
+
+    /**
+     * Set nbFreeSeats
+     *
+     * @param integer $nbFreeSeats
+     *
+     * @return Flight
+     */
+    public function setNbFreeSeats($nbFreeSeats)
+    {
+        $this->nbFreeSeats = $nbFreeSeats;
+
+        return $this;
+    }
+
+    /**
+     * Get nbFreeSeats
+     *
+     * @return int
+     */
+    public function getNbFreeSeats()
+    {
+        return $this->nbFreeSeats;
+    }
+
+    /**
+     * Set seatPrice
+     *
+     * @param float $seatPrice
+     *
+     * @return Flight
+     */
+    public function setSeatPrice($seatPrice)
+    {
+        $this->seatPrice = $seatPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get seatPrice
+     *
+     * @return float
+     */
+    public function getSeatPrice()
+    {
+        return $this->seatPrice;
+    }
+
+    /**
+     * Set takeOffTime
+     *
+     * @param \DateTime $takeOffTime
+     *
+     * @return Flight
+     */
+    public function setTakeOffTime($takeOffTime)
+    {
+        $this->takeOffTime = $takeOffTime;
+
+        return $this;
+    }
+
+    /**
+     * Get takeOffTime
+     *
+     * @return \DateTime
+     */
+    public function getTakeOffTime()
+    {
+        return $this->takeOffTime;
+    }
+
+    /**
+     * Set publicationDate
+     *
+     * @param \DateTime $publicationDate
+     *
+     * @return Flight
+     */
+    public function setPublicationDate($publicationDate)
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publicationDate
+     *
+     * @return \DateTime
+     */
+    public function getPublicationDate()
+    {
+        return $this->publicationDate;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Flight
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set pilot
+     *
+     * @param integer $pilot
+     *
+     * @return Flight
+     */
+    public function setPilot($pilot)
+    {
+        $this->pilot = $pilot;
+
+        return $this;
+    }
+
+    /**
+     * Get pilot
+     *
+     * @return int
+     */
+    public function getPilot()
+    {
+        return $this->pilot;
+    }
+
+    /**
+     * Set plane
+     *
+     * @param integer $plane
+     *
+     * @return Flight
+     */
+    public function setPlane($plane)
+    {
+        $this->plane = $plane;
+
+        return $this;
+    }
+
+    /**
+     * Get plane
+     *
+     * @return int
+     */
+    public function getPlane()
+    {
+        return $this->plane;
+    }
+
+    /**
+     * Set wasDone
+     *
+     * @param boolean $wasDone
+     *
+     * @return Flight
+     */
+    public function setWasDone($wasDone)
+    {
+        $this->wasDone = $wasDone;
+
+        return $this;
+    }
+
+    /**
+     * Get wasDone
+     *
+     * @return bool
+     */
+    public function getWasDone()
+    {
+        return $this->wasDone;
+    }
+}
+
