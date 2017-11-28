0fefa1613465ba622bb1b0f18f43b07d81a34b68
Maxime <xm.mx@laposte.net>
2017-11-28 20:42:35 +0100

    ajout methode form

---
+++ src/AppBundle/Form/ReviewType.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder1
+ * Date: 28/11/17
+ * Time: 20:34
+ */
+
+namespace AppBundle\Form;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Review;
+
+class ReviewType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('userRated')
+            ->add('text')
+            ->add('reviewAuthor')
+            ->add('note')
+            ->add('publicationDate');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Review'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_review';
+    }
+
+}
